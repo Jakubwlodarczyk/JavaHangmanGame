91c627a9cec2ac8b7466dd0362fe7a5aa0dcbe49
Jakub <kubawl.91@gmail.com>
2017-04-19 11:20:40 +0200

    Add method with list of capitals

--- Capitals.java
+++ Capitals.java
@@ -1,7 +1,28 @@
 package hangman;
 
+import java.util.ArrayList;
+
+
 public class Capitals{
+
+    public static ArrayList<String> capitalsList(){
+
+        List<String> capitalsList = new ArrayList<String>;
+        capitalsList = ["TIRANA", "ANDORRA LA VELLA", "YEREVAN", "VIENNA",
+                        "BAKU", "MINSK", "BRUSSELS", "SARAJEVO", "SOFIA",
+                        "ZAGREB", "NICOSIA", "PRAGUE", "COPENHAGEN", "TALLINN",
+                        "HELSINKI", "PARIS", "TBILISI", "BERLIN", "ATHENS",
+                        "BUDAPEST", "REYKJAVIK", "DUBLIN", "ROME", "ASTANA",
+                        "PRISTINA", "RIGA", "VADUZ", "VILNIUS", "LUXEMBOURG",
+                        "SKOPJE", "VALLETTA", "CHISINAU", "MONACO", "PODGORICA",
+                        "AMSTERDAM", "OSLO", "WARSAW", "LISBON", "BUCHAREST",
+                        "MOSCOW", "SAN MARINO", "BELGRADE", "BRATISLAVA",
+                        "LJUBLJANA", "MADRID", "STOCKHOLM", "BERN", "ANKARA",
+                        "KYIV", "LONDON", "VATICAN CITY"];
+        return capitalsList;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
