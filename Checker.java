bbce764841d6148627d43f9223c3dc43599bbb4c
Jakub <kubawl.91@gmail.com>
2017-04-19 13:36:46 +0200

    add new file converter - and move method to convert string to dashes to new file

--- Checker.java
+++ Checker.java
@@ -1,11 +1,6 @@
 
 public class Checker{
 
-    public static String convertToDash(String randomCapital){
-        String dashed;
-        dashed = new String(new char[randomCapital.length()]).replace("\0", "_ ");
-        return dashed;
-    }
 
     public static void main(String[] args) {
 
---
+++ Converter.java
@@ -0,0 +1,13 @@
+public class Converter{
+
+    public static String convertToDash(String randomCapital){
+        String dashed;
+        dashed = new String(new char[randomCapital.length()]).replace("\0", "_ ");
+        return dashed;
+    }
+
+    
+    public static void main(String[] args) {
+
+    }
+}
