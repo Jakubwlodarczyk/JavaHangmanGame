672ac84c02a487b39d7bee128ebff62044696dc9
Ania Polak <tunczykzcytryna@gmail.com>
2017-04-19 16:07:56 +0200

    Add new atribute to Game object - > random capital

--- Game.java
+++ Game.java
@@ -3,9 +3,11 @@ import java.util.ArrayList;
 
 public class Game{
     public ArrayList<String> usedLettersList;
+    public String currentCapital;
 
     public Game(){
         this.usedLettersList = new ArrayList<String>();
+        this.currentCapital = Capitals.getRandomCapital();
     }
 
     public ArrayList<String> getList(){
@@ -18,6 +20,8 @@ public class Game{
 
 
     public static void main(String[] args) {
+        // Game n = new Game();
+        // System.out.println(n.currentCapital);
 
     }
 }
