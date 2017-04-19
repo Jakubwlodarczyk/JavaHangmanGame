89e4458aae82c9bbbeb87b0db3926d9a539fe529
Ania Polak <tunczykzcytryna@gmail.com>
2017-04-19 13:27:10 +0200

    Player class created, compiles fine and works

--- Player.java
+++ Player.java
@@ -1,7 +1,22 @@
-package hangman;
-
 public class Player{
-    public static void main(String[] args) {
-        
+    private Integer life;
+
+    public Player(){
+        this.life = 5;
+    }
+    public Player(Integer life){
+        this.setLife(life);
+    }
+
+    public void setLife(Integer life){
+        this.life = life;
+    }
+
+    public void changeLife(Integer point){
+        this.life +=  point;
+    }
+
+    public Integer getLife(){
+            return life;
     }
 }
