d99e1a8f41e24d4c0cc4b55cdc8ebfbe6e6a0718
Jakub <kubawl.91@gmail.com>
2017-04-19 14:03:21 +0200

    add check method to check answer (a letter, and whole word)

--- Checker.java
+++ Checker.java
@@ -1,6 +1,17 @@
 
 public class Checker{
 
+    public static Boolean haveLetter(String userInput, String randomCapital){
+        Boolean haveLetter = new Boolean(false);
+        if (randomCapital.indexOf(userInput) >= 0){
+            haveLetter = true;
+        }
+        return haveLetter;
+    }
+
+    public static Boolean isCorrectAnswer(String userInput, String randomCapital){
+        return randomCapital.equals(userInput);
+    }
 
     public static void main(String[] args) {
 
--- Converter.java
+++ Converter.java
@@ -7,6 +7,7 @@ public class Converter{
     }
 
     
+
     public static void main(String[] args) {
 
     }
