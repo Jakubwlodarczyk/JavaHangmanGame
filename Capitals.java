272b37d05b3b58e67743684ab7b3c67d693893f5
Ania Polak <tunczykzcytryna@gmail.com>
2017-04-19 11:22:37 +0200

    Add getRandomCapital method

--- Capitals.java
+++ Capitals.java
@@ -1,7 +1,14 @@
 package hangman;
+import java.util.Random;
 
 public class Capitals{
+
+    public static String getRandomCapital(List<String> capitals) {
+        String randomCapital = capitals.get(new Random().nextInt(list.size()));
+        return randomCapital;
+
+    }
     public static void main(String[] args) {
-        
+
     }
 }
